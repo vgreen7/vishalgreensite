a9e636ce4359c11860d6f5cba0a360563c112936
Vishal Green <vishalgreen@vt.edu>
2024-08-05 12:39:04 -0400

    Added mobile optimization

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,26 +1,59 @@
+"use client";
+
+import { useState } from "react";
 import Link from "next/link";
 import styles from "/styles/Navbar.module.css";
 
 const Navbar = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.navbar}>
-      <ul className={styles.navList}>
-        <li className={styles.navItem}>
-          <Link href="/">Home</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/about">About Me</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/work-experience">Work Experience</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/projects">Projects</Link>
-        </li>
-        <li className={styles.navItem}>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
+      <div className={styles.brand}>
+        <Link href="/">My Resume</Link>
+      </div>
+      <div className={styles.hamburger} onClick={toggleMenu}>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+        <div className={styles.bar}></div>
+      </div>
+      <div className={`${styles.menu} ${isOpen ? styles.show : ""}`}>
+        <Link href="/" className={styles.link} onClick={() => setIsOpen(false)}>
+          Home
+        </Link>
+        <Link
+          href="/about"
+          className={styles.link}
+          onClick={() => setIsOpen(false)}
+        >
+          About
+        </Link>
+        <Link
+          href="/work-experience"
+          className={styles.link}
+          onClick={() => setIsOpen(false)}
+        >
+          Work Experience
+        </Link>
+        <Link
+          href="/projects"
+          className={styles.link}
+          onClick={() => setIsOpen(false)}
+        >
+          Projects
+        </Link>
+        <Link
+          href="/contact"
+          className={styles.link}
+          onClick={() => setIsOpen(false)}
+        >
+          Contact
+        </Link>
+      </div>
     </nav>
   );
 };
